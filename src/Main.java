4b052891de31c4f49d449be2fe02594893538eca
FinnT730 <finnt730@gmail.com>
2019-10-30 12:20:54 +0100

    Vector math? maybe.....

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 import com.sun.javafx.geom.Vec3f;
 import com.sun.javafx.geom.Vec4f;
+import javafx.scene.Camera;
+import javafx.scene.ParallelCamera;
 
 import javax.swing.*;
-import javax.vecmath.Tuple3f;
-import javax.vecmath.Vector3f;
+import javax.vecmath.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
@@ -33,22 +34,32 @@ public class Main extends Component implements Runnable, KeyListener, MouseListe
     }
 
 
-    Vec3f vec = new Vec3f(50, 5, 0);
+    Matrix3f matrix = new Matrix3f();
+
+    Vec3f vec = new Vec3f(0, 0, 0);
 
     @Override
     public void paint(Graphics g) {
         super.paint(g);
-        Vec3f vec1 = new Vec3f(50, 100, 300);
+        Vec3f vec1 = new Vec3f(0, 0, 300);
         Vec3f res = new Vec3f(100, 200, 2);
 
 
         Vector3f vector = new Vector3f(vec.x, vec.y, vec.z);
         Tuple3f tup = new Vector3f(0, 0, 0);
 
+        tup.set(vec1.x,vec1.y,vec1.z);
+        Point3f point = new Point3f(tup);
+
+
+        matrix.set(new Quat4f(10,10,50,50));
+
+
+        point.project(new Point4f(vec.x,vec.y,vec.z,vec1.x));
 
 //        vec.dot(vec1);
 
-        g.drawRect((int) vec.x, (int) vec.y, (int) vec1.x, (int) vec1.y);
+        g.drawRect((int) vec.x, (int) vec.y, (int) matrix.getM00(), (int) matrix.getM01());
 //        g.draw3DRect((int)vec.x,(int)vec.y,(int)vec1.x,(int)vec1.y,false);
 
 //        System.out.println(vec);
@@ -99,25 +110,25 @@ public class Main extends Component implements Runnable, KeyListener, MouseListe
 
     @Override
     public void keyTyped(final KeyEvent e) {
-
+        System.out.println(matrix.toString());
     }
 
     @Override
     public void keyPressed(final KeyEvent e) {
         if (e.getKeyChar() == 'a') {
-            vec.add(new Vec3f(-1, 0, 0));
+            vec.add(new Vec3f(-10, 0, 0));
             repaint();
         }
         if (e.getKeyChar() == 'w') {
-            vec.add(new Vec3f(0, -1, 0));
+            vec.add(new Vec3f(0, -10, 0));
             repaint();
         }
         if (e.getKeyChar() == 's') {
-            vec.add(new Vec3f(0, 1, 0));
+            vec.add(new Vec3f(0, 10, 0));
             repaint();
         }
         if (e.getKeyChar() == 'd') {
-            vec.add(new Vec3f(1, 0, 0));
+            vec.add(new Vec3f(10, 0, 0));
             repaint();
         }
 
@@ -131,6 +142,12 @@ public class Main extends Component implements Runnable, KeyListener, MouseListe
             }
 
             app.getGraphics().dispose();
+        }
+
+
+        if(e.getKeyChar() == '-') {
+            canRun = true;
+            thread.run();
         }
 
         update(this.getGraphics());
